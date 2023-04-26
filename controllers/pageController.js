4cdada0a85a102b245c563bb572ae7b5b3ac9674
zqjo <zqjo@fresnou.org>
2023-04-26 11:58:49 -0700

    doing..! ,:)
    (i am doing both charts and employee manager at once, and hoo boy why is this so challenging-)

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,38 +1,47 @@
+const {Role, User} = require('../models');
+
 module.exports.renderAccounting = function(req, res){
     if (!req.user.can('view accounting')) {
         res.redirect('/');
         return;
     }
     res.render('pages/accounting');
-}
+};
+
 module.exports.renderDashboard = function(req, res){
     if (!req.user.can('view dashboard')) {
         res.redirect('/');
         return;
     }
     res.render('pages/dashboard');
-}
-module.exports.renderHR = function(req, res){
+};
+
+module.exports.renderHR = async function(req, res){
+    console.log(req.user);
     if (!req.user.can('view hr')) {
         res.redirect('/');
         return;
     }
-    res.render('pages/hr');
-}
+    const users = await User.findAll();
+    res.render('pages/hr', {users});
+};
+
 module.exports.renderMarketing = function(req, res){
     if (!req.user.can('view marketing')) {
         res.redirect('/');
         return;
     }
     res.render('pages/marketing');
-}
+};
+
 module.exports.renderSales = function(req, res){
     if (!req.user.can('view sales')) {
         res.redirect('/');
         return;
     }
     res.render('pages/sales');
-}
+};
+
 module.exports.viewProfile = function(req, res) {
     res.render('pages/profile');
-}
+};
